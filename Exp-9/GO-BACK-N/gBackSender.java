4d266d6f5ce8e038bedd2f8c488b55aa028487a3
Rahul Gedela <106228898+rahulxtpbg@users.noreply.github.com>
2022-06-27 16:12:39 +0530

    All Hail Megatron!

---
+++ Exp-9/GO-BACK-N/gBackSender.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+class Sender {
+    public static void main(String args[]) throws Exception {
+        Sender s = new Sender();
+        s.run();
+    }
+
+    public void run() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no: of frames to be sent:");
+        int n = sc.nextInt();
+        Socket s = new Socket("localhost", 9999);
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        for (int i = 0; i < n; i++) {
+            if (i == n) {
+                ps.println("Exit");
+                break;
+            }
+            System.out.println("Frame" + " " + i + " " + "has been sent");
+            ps.println(i);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String ack = br.readLine();
+            if (ack != "null") {
+                System.out.println("Acknowledgement received");
+            } else {
+                System.out.println("Acknowledgement for frame" + " " + i + " " + "not received");
+                System.out.println("Resending from frame" + " " + i);
+            }
+        }
+    }
+}
